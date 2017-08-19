e09289315c5eeac68f7885076f3996f0012ce8f4
Chris <sjhshy@gmail.com>
2017-08-19 23:51:20 +0900

    Initial commit

---
+++ enjoy_mineral_shards.py
@@ -0,0 +1,53 @@
+import sc2_deepq
+
+from baselines import deepq
+
+import gflags as flags
+import sys
+
+from pysc2.env import sc2_env
+
+import baselines.common.tf_util as U
+
+step_mul = 16
+steps = 200
+
+FLAGS = flags.FLAGS
+
+def main():
+  FLAGS(sys.argv)
+  with sc2_env.SC2Env(
+      "CollectMineralShards",
+      step_mul=step_mul,
+      visualize=True,
+      game_steps_per_episode=steps * step_mul) as env:
+
+    model = deepq.models.cnn_to_mlp(
+      convs=[(32, 8, 4), (64, 4, 2), (64, 3, 1)],
+      hiddens=[256],
+      dueling=True
+    )
+
+    def make_obs_ph(name):
+      return U.BatchInput(env.observation_spec()["screen"], name=name)
+
+    act_params = {
+      'make_obs_ph': make_obs_ph,
+      'q_func': model,
+      'num_actions': 64 * 64,
+    }
+
+    act = sc2_deepq.load("sc2_mineral_shards.pkl", act_params=act_params)
+
+    while True:
+      obs, done = env.reset(), False
+      episode_rew = 0
+      while not done:
+        env.render()
+        obs, rew, done, _ = env.step(act(obs[None])[0])
+        episode_rew += rew
+      print("Episode reward", episode_rew)
+
+
+if __name__ == '__main__':
+  main()
---
+++ train_mineral_shards.py
@@ -0,0 +1,55 @@
+import sc2_deepq
+from baselines import deepq
+
+from pysc2.env import sc2_env
+from pysc2.lib import actions
+
+import gflags as flags
+import sys
+from s2clientprotocol import sc2api_pb2
+
+
+_MOVE_SCREEN = actions.FUNCTIONS.Move_screen.id
+_SELECT_ARMY = actions.FUNCTIONS.select_army.id
+_SELECT_ALL = [0]
+_NOT_QUEUED = [0]
+
+step_mul = 16
+steps = 200
+
+FLAGS = flags.FLAGS
+
+def main():
+  FLAGS(sys.argv)
+  with sc2_env.SC2Env(
+      "CollectMineralShards",
+      step_mul=step_mul,
+      visualize=True,
+      game_steps_per_episode=steps * step_mul) as env:
+
+    model = deepq.models.cnn_to_mlp(
+      convs=[(3, 1, 1)],
+      hiddens=[256],
+      dueling=True
+    )
+
+    act = sc2_deepq.learn(
+      env,
+      q_func=model,
+      num_actions=64 * 64,
+      lr=1e-4,
+      max_timesteps=2000000,
+      buffer_size=10000,
+      exploration_fraction=0.001,
+      exploration_final_eps=0.5,
+      train_freq=4,
+      learning_starts=10000,
+      target_network_update_freq=1000,
+      gamma=0.99,
+      prioritized_replay=True
+    )
+    act.save("sc2_mineral_shards_2mil.pkl")
+
+
+if __name__ == '__main__':
+  main()
