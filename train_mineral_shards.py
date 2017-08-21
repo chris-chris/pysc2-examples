625aaeee7db54c59b63ca3482dd5e85b8e4ee7aa
Chris <sjhshy@gmail.com>
2017-08-22 01:14:23 +0900

    increase exploration_fraction to 1

--- train_mineral_shards.py
+++ train_mineral_shards.py
@@ -40,7 +40,7 @@ def main():
       lr=1e-4,
       max_timesteps=2000000,
       buffer_size=10000,
-      exploration_fraction=0.1,
+      exploration_fraction=1,
       exploration_final_eps=0.01,
       train_freq=4,
       learning_starts=10000,
