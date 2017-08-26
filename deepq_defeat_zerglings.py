6427834a1a0324c978926cac1d90719e6cbded73
Chris <sjhshy@gmail.com>
2017-08-27 03:57:35 +0900

    fix grouping algo

--- deepq_defeat_zerglings.py
+++ deepq_defeat_zerglings.py
@@ -250,7 +250,7 @@ def learn(env,
 
   obs = init(env, player_relative, obs)
 
-  #group_id = 0
+  group_id = 0
   reset = True
   with tempfile.TemporaryDirectory() as td:
     model_saved = False
@@ -279,7 +279,11 @@ def learn(env,
         kwargs['update_param_noise_threshold'] = update_param_noise_threshold
         kwargs['update_param_noise_scale'] = True
 
-      #group_list = update_group_list(obs)
+      group_list = update_group_list(obs)
+
+      if(check_group_list(env, obs)):
+        obs = init(env, player_relative, obs)
+        group_list = update_group_list(obs)
 
       # if(len(group_list) == 0):
       #   obs = init(env, player_relative, obs)
@@ -309,40 +313,26 @@ def learn(env,
       else:
 
         # If there is no marine in danger, select random
+        while(len(group_list)>0):
+          # units = env._obs.observation.raw_data.units
+          # marine_list = []
+          # for unit in units:
+          #   if(unit.alliance == 1):
+          #     marine_list.append(unit)
 
-        player_y, player_x = (player_relative == _PLAYER_FRIENDLY).nonzero()
+          group_id = np.random.choice(group_list)
+          #xy = [int(unit.pos.y - 10), int(unit.pos.x+8)]
+          #print("check xy : %s - %s" % (xy, player_relative[xy[0],xy[1]]))
+          obs = env.step(actions=[sc2_actions.FunctionCall(_SELECT_CONTROL_GROUP, [[_CONTROL_GROUP_RECALL], [group_id]])])
 
-        for i in range(len(player_x)):
-          if i % 4 != 0:
-            continue
+          selected = obs[0].observation["screen"][_SELECTED]
+          player_y, player_x = (selected == _PLAYER_FRIENDLY).nonzero()
+          if(len(player_y)>0):
+            player = [int(player_x.mean()), int(player_y.mean())]
+            break
+          else:
+            group_list.remove(group_id)
 
-          xy = [player_x[i], player_y[i]]
-          print("real marine xy : %s" % xy)
-
-        units = env._obs.observation.raw_data.units
-        marine_list = []
-        for unit in units:
-          if(unit.alliance == 1):
-            marine_list.append(unit)
-            print("marine xy : [%s, %s]" % (unit.pos.x, unit.pos.y) )
-
-            xy = [int((unit.pos.x - 21) / 0.375), int(((unit.pos.y - 13)) / 0.5 )]
-            print("check xy : %s - %s" % (xy, player_relative[xy[0],xy[1]]))
-
-        unit = np.random.choice(marine_list)
-        xy = [int((unit.pos.x - 21) / 0.375), int(((unit.pos.y - 13)) / 0.5  )]
-        obs = env.step(actions=[sc2_actions.FunctionCall(_SELECT_POINT, [[0], xy])])
-
-        selected = obs[0].observation["screen"][_SELECTED]
-        player_y, player_x = (selected == _PLAYER_FRIENDLY).nonzero()
-        if(len(player_y)>0):
-          player = [int(player_x.mean()), int(player_y.mean())]
-      # else:
-      #   group_list.remove(group_id)
-
-      # if(check_group_list(env, obs)):
-      #   obs = init(env, player_relative, obs)
-      #   group_list = update_group_list(obs)
 
 
       if(player[0]>32):
@@ -374,7 +364,7 @@ def learn(env,
 
         new_action = [sc2_actions.FunctionCall(_NO_OP, [])]
 
-      elif(action == 0): #Attack
+      elif(action <= 1): #Attack
 
         # nearest enemy
 
@@ -384,7 +374,7 @@ def learn(env,
 
         #print("action : %s Attack Coord : %s" % (action, coord))
 
-      elif(action == 1): # Oppsite direcion from enemy
+      elif(action == 2): # Oppsite direcion from enemy
 
         # nearest enemy opposite
 
@@ -473,7 +463,7 @@ def learn(env,
 
         screen = player_relative
 
-        obs = init(env, player_relative, obs)
+        group_list = init(env, player_relative, obs)
 
         # Select all marines first
         #env.step(actions=[sc2_actions.FunctionCall(_SELECT_UNIT, [_SELECT_ALL])])
@@ -529,6 +519,12 @@ def init(env, player_relative, obs):
   if(army_count==0):
     return obs
 
+  obs = env.step(actions=[sc2_actions.FunctionCall(_NO_OP, [])])
+  obs = env.step(actions=[sc2_actions.FunctionCall(_NO_OP, [])])
+  obs = env.step(actions=[sc2_actions.FunctionCall(_NO_OP, [])])
+  obs = env.step(actions=[sc2_actions.FunctionCall(_NO_OP, [])])
+  obs = env.step(actions=[sc2_actions.FunctionCall(_NO_OP, [])])
+
   player_y, player_x = (player_relative == _PLAYER_FRIENDLY).nonzero()
   obs = env.step(actions=[sc2_actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL])])
 
@@ -539,44 +535,44 @@ def init(env, player_relative, obs):
     xy = [player_x[i], player_y[i]]
     obs = env.step(actions=[sc2_actions.FunctionCall(_SELECT_POINT, [[0], xy])])
 
-  # group_id = 0
-  # group_list = []
-  # unit_xy_list = []
-  # for i in range(len(player_x)):
-  #   if i % 4 != 0:
-  #     continue
-  #
-  #   if group_id > 9:
-  #     break
-  #
-  #   xy = [player_x[i], player_y[i]]
-  #   unit_xy_list.append(xy)
-  #
-  #   if(len(unit_xy_list) >= 1):
-  #     for idx, xy in enumerate(unit_xy_list):
-  #       if(idx==0):
-  #         obs = env.step(actions=[sc2_actions.FunctionCall(_SELECT_POINT, [[0], xy])])
-  #       else:
-  #         obs = env.step(actions=[sc2_actions.FunctionCall(_SELECT_POINT, [[1], xy])])
-  #
-  #     obs = env.step(actions=[sc2_actions.FunctionCall(_SELECT_CONTROL_GROUP, [[_CONTROL_GROUP_SET], [group_id]])])
-  #     unit_xy_list = []
-  #
-  #     group_list.append(group_id)
-  #     group_id += 1
-  #
-  # if(len(unit_xy_list) >= 1):
-  #   for idx, xy in enumerate(unit_xy_list):
-  #     if(idx==0):
-  #       obs = env.step(actions=[sc2_actions.FunctionCall(_SELECT_POINT, [[0], xy])])
-  #     else:
-  #       obs = env.step(actions=[sc2_actions.FunctionCall(_SELECT_POINT, [[1], xy])])
-  #
-  #   obs = env.step(actions=[sc2_actions.FunctionCall(_SELECT_CONTROL_GROUP, [[_CONTROL_GROUP_SET], [group_id]])])
-  #
-  #   group_list.append(group_id)
-  #   group_id += 1
-  #
+  group_id = 0
+  group_list = []
+  unit_xy_list = []
+  for i in range(len(player_x)):
+    if i % 4 != 0:
+      continue
+
+    if group_id > 9:
+      break
+
+    xy = [player_x[i], player_y[i]]
+    unit_xy_list.append(xy)
+
+    if(len(unit_xy_list) >= 1):
+      for idx, xy in enumerate(unit_xy_list):
+        if(idx==0):
+          obs = env.step(actions=[sc2_actions.FunctionCall(_SELECT_POINT, [[0], xy])])
+        else:
+          obs = env.step(actions=[sc2_actions.FunctionCall(_SELECT_POINT, [[1], xy])])
+
+      obs = env.step(actions=[sc2_actions.FunctionCall(_SELECT_CONTROL_GROUP, [[_CONTROL_GROUP_SET], [group_id]])])
+      unit_xy_list = []
+
+      group_list.append(group_id)
+      group_id += 1
+
+  if(len(unit_xy_list) >= 1):
+    for idx, xy in enumerate(unit_xy_list):
+      if(idx==0):
+        obs = env.step(actions=[sc2_actions.FunctionCall(_SELECT_POINT, [[0], xy])])
+      else:
+        obs = env.step(actions=[sc2_actions.FunctionCall(_SELECT_POINT, [[1], xy])])
+
+    obs = env.step(actions=[sc2_actions.FunctionCall(_SELECT_CONTROL_GROUP, [[_CONTROL_GROUP_SET], [group_id]])])
+
+    group_list.append(group_id)
+    group_id += 1
+
   return obs
 
 def update_group_list(obs):
--- train_defeat_zerglings.py
+++ train_defeat_zerglings.py
@@ -13,7 +13,7 @@ _SELECT_ALL = [0]
 _NOT_QUEUED = [0]
 
 step_mul = 1
-steps = 2000
+steps = 800
 
 FLAGS = flags.FLAGS
 
@@ -34,7 +34,7 @@ def main():
     act = deepq_defeat_zerglings.learn(
       env,
       q_func=model,
-      num_actions=2,
+      num_actions=3,
       lr=1e-4,
       max_timesteps=10000000,
       buffer_size=100000,
