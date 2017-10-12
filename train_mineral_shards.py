8e94dae270adcf29fc929cf640028a8446bf59cd
Chris <sjhshy@gmail.com>
2017-10-12 20:46:47 +0900

    add callback to train_mineral_shards.py

--- train_mineral_shards.py
+++ train_mineral_shards.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 import gflags as flags
 from baselines import deepq
@@ -38,6 +39,13 @@ flags.DEFINE_boolean("prioritized", True, "prioritized_replay")
 flags.DEFINE_boolean("dueling", True, "dueling")
 flags.DEFINE_float("lr", 0.0005, "Learning rate")
 flags.DEFINE_integer("num_cpu", 4, "number of cpus")
+
+PROJ_DIR = os.path.dirname(os.path.abspath(__file__))
+
+max_mean_reward = 0
+last_filename = ""
+
+start_time = datetime.datetime.now().strftime("%Y%m%d%H%M")
 
 def main():
   FLAGS(sys.argv)
@@ -108,7 +116,8 @@ def main():
         learning_starts=10000,
         target_network_update_freq=1000,
         gamma=0.99,
-        prioritized_replay=True
+        prioritized_replay=True,
+        callback=deepq_callback
       )
       act.save("mineral_shards.pkl")
 
@@ -151,7 +160,7 @@ def main():
     env = SubprocVecEnv(FLAGS.num_cpu, FLAGS.map)
 
     policy_fn = CnnPolicy
-    acktr_disc.learn(policy_fn, env, seed, total_timesteps=num_timesteps, nprocs=FLAGS.num_cpu)
+    acktr_disc.learn(policy_fn, env, seed, total_timesteps=num_timesteps, nprocs=FLAGS.num_cpu, callback=acktr_callback)
 
 from pysc2.env import environment
 import numpy as np
@@ -206,6 +215,73 @@ class AgentController(object):
       dones.append(done)
       infos.append(info)
     return np.stack(obs), np.stack(rewards), np.stack(dones), np.stack(infos)
+
+def deepq_callback(locals, globals):
+  #pprint.pprint(locals)
+  global max_mean_reward, last_filename
+  if('done' in locals and locals['done'] == True):
+    if('mean_100ep_reward' in locals
+       and locals['num_episodes'] >= 10
+       and locals['mean_100ep_reward'] > max_mean_reward
+       ):
+      print("mean_100ep_reward : %s max_mean_reward : %s" %
+            (locals['mean_100ep_reward'], max_mean_reward))
+
+      if(not os.path.exists(os.path.join(PROJ_DIR,'models/deepq/'))):
+        try:
+          os.mkdir(os.path.join(PROJ_DIR,'models/'))
+        except Exception as e:
+          print(str(e))
+        try:
+          os.mkdir(os.path.join(PROJ_DIR,'models/deepq/'))
+        except Exception as e:
+          print(str(e))
+
+      if(last_filename != ""):
+        os.remove(last_filename)
+        print("delete last model file : %s" % last_filename)
+
+      max_mean_reward = locals['mean_100ep_reward']
+      act = deepq.ActWrapper(locals['act'], locals['act_params'])
+
+      filename = os.path.join(PROJ_DIR,'models/deepq/zergling_%s.pkl' % locals['mean_100ep_reward'])
+      act.save(filename)
+      print("save best mean_100ep_reward model to %s" % filename)
+      last_filename = filename
+
+def acktr_callback(locals, globals):
+  global max_mean_reward, last_filename
+  #pprint.pprint(locals)
+
+  if('mean_100ep_reward' in locals
+     and locals['num_episodes'] >= 10
+     and locals['mean_100ep_reward'] > max_mean_reward
+     ):
+    print("mean_100ep_reward : %s max_mean_reward : %s" %
+          (locals['mean_100ep_reward'], max_mean_reward))
+
+    if(not os.path.exists(os.path.join(PROJ_DIR,'models/acktr/'))):
+      try:
+        os.mkdir(os.path.join(PROJ_DIR,'models/'))
+      except Exception as e:
+        print(str(e))
+      try:
+        os.mkdir(os.path.join(PROJ_DIR,'models/acktr/'))
+      except Exception as e:
+        print(str(e))
+
+    if(last_filename != ""):
+      os.remove(last_filename)
+      print("delete last model file : %s" % last_filename)
+
+    max_mean_reward = locals['mean_100ep_reward']
+    model = locals['model']
+
+    filename = os.path.join(PROJ_DIR,'models/acktr/zergling_%s.pkl' % locals['mean_100ep_reward'])
+    model.save(filename)
+    print("save best mean_100ep_reward model to %s" % filename)
+    last_filename = filename
+
 
 if __name__ == '__main__':
   main()
