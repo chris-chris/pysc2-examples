d6c383bb9e5e7edf5439b2fdee33feab2100af7d
Chris <sjhshy@gmail.com>
2017-10-29 11:22:10 +0900

    comment out CloudpickleWrapper

--- common/vec_env/subproc_vec_env.py
+++ common/vec_env/subproc_vec_env.py
@@ -56,18 +56,18 @@ def worker(remote, map_name):
             else:
                 raise NotImplementedError
 
-class CloudpickleWrapper(object):
-    """
-    Uses cloudpickle to serialize contents (otherwise multiprocessing tries to use pickle)
-    """
-    def __init__(self, x):
-        self.x = x
-    def __getstate__(self):
-        import cloudpickle
-        return cloudpickle.dumps(self.x)
-    def __setstate__(self, ob):
-        import pickle
-        self.x = pickle.loads(ob)
+# class CloudpickleWrapper(object):
+#     """
+#     Uses cloudpickle to serialize contents (otherwise multiprocessing tries to use pickle)
+#     """
+#     def __init__(self, x):
+#         self.x = x
+#     def __getstate__(self):
+#         import cloudpickle
+#         return cloudpickle.dumps(self.x)
+#     def __setstate__(self, ob):
+#         import pickle
+#         self.x = pickle.loads(ob)
 
 class SubprocVecEnv(VecEnv):
     def __init__(self, nenvs, map_name):
